ea91b762c6ccadea04829e21dcbe31d95319428d
Ashwin.D.S <astroashwin@outlook.com>
2021-09-03 08:30:48 +0530

    Post-Bot: update email and username for git commits
    we now have our account for bot related functions
    @ProjectSakuraBot
    
    Co-Authored-By: Project Sakura Bot <90018070+ProjectSakuraBot@users.noreply.github.com>
    Signed-off-by: Ashwin.D.S <astroashwin@outlook.com>

--- .github/scripts/push.sh
+++ .github/scripts/push.sh
@@ -1,5 +1,5 @@
-git config --global user.email "<>"
-git config --global user.name "Sakura OTA poster"
+git config --global user.email "noreply.projectsakurabot@gmail.com"
+git config --global user.name "Project Sakura Bot"
 git fetch
 git pull
 COMMIT_MESSAGE=$(cat commit_mesg.txt)
